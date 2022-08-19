e4981e692fc1787be1c6e7b654d8aa71925b1034
scouri <eneblank19@protonmail.com>
2022-08-19 01:14:45 +0000

    Update compress.clj

--- src/compressor/compress.clj
+++ src/compressor/compress.clj
@@ -1,24 +1,23 @@
 (ns compressor.compress
-  (:import [java.awt.Image BufferedImage]
-           [clojure.java.io File]
-           [javax.imageio ImageIO IIOImage]
+  (:require [clojure.java.io :as io])
+  (:import [javax.imageio ImageIO IIOImage]
            [javax.imageio.plugins.jpeg JPEGImageWriteParam]))
 
 (defn compress-jpg
   [jpg & {:keys [quality name]
           :or {quality 0.7 name jpg}}]
   (let [reader (.next (ImageIO/getImageReadersByFormatName "jpg"))
-        writer (.next (ImageIO/getImageWritersByFormatName "jpg"))
-        metadata (IIOImage. (.read reader 0) nil
-                            (.getImageMetadata reader 0))
-        params (doto (.getDefaultWriteParm writer)
-                 (.setCompressionMode JPEGImageWriteParam/MODE_EXPLICIT)
-                 (.setCompressionQuality quality))]
-    (with-open [input-stream (ImageIO/createImageInputStream (File. jpg))
-                output-stream (ImageIO/createImageOutputStream (File. name))]
+        writer (.next (ImageIO/getImageWritersByFormatName "jpg"))]
+    (with-open [input-stream (ImageIO/createImageInputStream (io/file jpg))
+                output-stream (ImageIO/createImageOutputStream (io/file name))]
       (.setInput reader input-stream)
-      (.setOutput writer output-stream))
-    (.write writer nil metadata params)
-    (finally
-      (do (.dispose writer)
-          (.dispose reader)))))
+      (.setOutput writer output-stream)
+    (let [metadata (IIOImage. (.read reader 0) nil
+                              (.getImageMetadata reader 0))
+          params (doto (.getDefaultWriteParam writer)
+                   (.setCompressionMode JPEGImageWriteParam/MODE_EXPLICIT)
+                   (.setCompressionQuality (read-string quality))
+                   (.setOptimizeHuffmanTables true))]
+    (.write writer nil metadata params))
+    (.dispose reader)
+    (.dispose writer))))
