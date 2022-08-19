551f51f2f18567a9609e0150d92e960a0db523db
scouri <eneblank19@protonmail.com>
2022-08-19 04:57:46 +0000

    potential shift to faster library soon

--- src/compressor/compress.clj
+++ src/compressor/compress.clj
@@ -1,13 +1,18 @@
 (ns compressor.compress
   (:require [clojure.java.io :as io])
-  (:import [javax.imageio ImageIO IIOImage]
-           [javax.imageio.plugins.jpeg JPEGImageWriteParam]))
+  (:import [javax.imageio ImageIO IIOImage ImageWriteParam]
+           ))
+
+(defn file-extension
+  [file]
+  (second (re-find #"([^.][a-zA-Z0-9]+)$" file)))
 
 (defn compress-jpg
   [jpg & {:keys [quality name]
           :or {quality 0.7 name jpg}}]
-  (let [reader (.next (ImageIO/getImageReadersByFormatName "jpg"))
-        writer (.next (ImageIO/getImageWritersByFormatName "jpg"))]
+  (let [ext (file-extension jpg)
+        reader (.next (ImageIO/getImageReadersByFormatName ext))
+        writer (.next (ImageIO/getImageWritersByFormatName ext))]
     (with-open [input-stream (ImageIO/createImageInputStream (io/file jpg))
                 output-stream (ImageIO/createImageOutputStream (io/file name))]
       (.setInput reader input-stream)
@@ -15,9 +20,14 @@
     (let [metadata (IIOImage. (.read reader 0) nil
                               (.getImageMetadata reader 0))
           params (doto (.getDefaultWriteParam writer)
-                   (.setCompressionMode JPEGImageWriteParam/MODE_EXPLICIT)
+                   (.setCompressionMode ImageWriteParam/MODE_EXPLICIT)
                    (.setCompressionQuality (read-string quality))
                    (.setOptimizeHuffmanTables true))]
     (.write writer nil metadata params))
     (.dispose reader)
     (.dispose writer))))
+
+(defn compress-jpg-rewrite
+  [jpg & {:keys [quality name]
+          :or {quality 0.7 name jpg}}]
+  )
