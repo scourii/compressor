66ca778385d87ea2fb9aee9fd724f366e8617a88
scouri <eneblank19@protonmail.com>
2022-08-20 04:07:14 +0000

    speed optimization(10ms), metadata toggle

--- src/compressor/compress.clj
+++ src/compressor/compress.clj
@@ -1,33 +1,25 @@
 (ns compressor.compress
   (:require [clojure.java.io :as io])
-  (:import [javax.imageio ImageIO IIOImage ImageWriteParam]
-           ))
+  (:import [javax.imageio ImageIO IIOImage ImageWriteParam]))
 
 (defn file-extension
   [file]
   (second (re-find #"([^.][a-zA-Z0-9]+)$" file)))
 
-(defn compress-jpg
-  [jpg & {:keys [quality name]
-          :or {quality 0.7 name jpg}}]
-  (let [ext (file-extension jpg)
+(defn compress-img
+  [img & {:keys [quality name metadata]
+          :or {quality 0.7 name img metadata false}}]
+  (let [ext (file-extension img)
         reader (.next (ImageIO/getImageReadersByFormatName ext))
         writer (.next (ImageIO/getImageWritersByFormatName ext))]
-    (with-open [input-stream (ImageIO/createImageInputStream (io/file jpg))
+    (with-open [input-stream (ImageIO/createImageInputStream (io/file img))
                 output-stream (ImageIO/createImageOutputStream (io/file name))]
       (.setInput reader input-stream)
       (.setOutput writer output-stream)
-    (let [metadata (IIOImage. (.read reader 0) nil
-                              (.getImageMetadata reader 0))
-          params (doto (.getDefaultWriteParam writer)
-                   (.setCompressionMode ImageWriteParam/MODE_EXPLICIT)
-                   (.setCompressionQuality (read-string quality))
-                   (.setOptimizeHuffmanTables true))]
-    (.write writer nil metadata params))
-    (.dispose reader)
-    (.dispose writer))))
-
-(defn compress-jpg-rewrite
-  [jpg & {:keys [quality name]
-          :or {quality 0.7 name jpg}}]
-  )
+      (let [image (if metadata (IIOImage. (.read reader 0) nil (.getImageMetadata reader 0)) (IIOImage. (.read reader 0) nil nil))
+            params (doto (.getDefaultWriteParam writer)
+                     (.setCompressionMode ImageWriteParam/MODE_EXPLICIT)
+                     (.setCompressionQuality (read-string quality)))]
+        (.write writer nil image params))
+      (.dispose reader)
+      (.dispose writer))))
