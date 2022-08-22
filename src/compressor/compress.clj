f8fbcd0be4108a9357b709e1a921795468e2cef6
scouri <eneblank19@protonmail.com>
2022-08-22 18:38:37 +0000

    todo: supporting directory compression

--- src/compressor/compress.clj
+++ src/compressor/compress.clj
@@ -7,19 +7,20 @@
   (second (re-find #"([^.][a-zA-Z0-9]+)$" file)))
 
 (defn compress-img
-  [img & {:keys [quality name metadata]
-          :or {quality 0.7 name img metadata false}}]
+  [img & {:keys [quality new metadata]
+          :or {quality 0.7 new img metadata false}}]
   (let [ext (file-extension img)
         reader (.next (ImageIO/getImageReadersByFormatName ext))
         writer (.next (ImageIO/getImageWritersByFormatName ext))]
     (with-open [input-stream (ImageIO/createImageInputStream (io/file img))
-                output-stream (ImageIO/createImageOutputStream (io/file name))]
+                output-stream (ImageIO/createImageOutputStream (io/file new))]
       (.setInput reader input-stream)
       (.setOutput writer output-stream)
       (let [image (if metadata (IIOImage. (.read reader 0) nil (.getImageMetadata reader 0)) (IIOImage. (.read reader 0) nil nil))
             params (doto (.getDefaultWriteParam writer)
                      (.setCompressionMode ImageWriteParam/MODE_EXPLICIT)
-                     (.setCompressionQuality (read-string quality)))]
+                     (.setCompressionQuality (read-string quality))
+                     (.setOptimizeHuffmanTables true))]
         (.write writer nil image params))
       (.dispose reader)
       (.dispose writer))))
