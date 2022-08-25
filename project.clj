43a196367fb618e88217a01249b557fb9e22bf66
scouri <eneblank19@protonmail.com>
2022-08-25 02:21:04 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject compressor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject compressor "0.1"
+  :description "Image Compressor"
+  :url "https://github.com/scourii/compressor"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :main ^:skip-aot compressor.core
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.cli "1.0.206"]]
   :repl-options {:init-ns compressor.core}
   :target-path "target/%s" 
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :aliases {}
+  :main compressor.core
+  :aot [compressor.core compressor.compress])
