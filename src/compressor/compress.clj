7960a83d51ad8dbd5cdf0083e93c89c42e99ccbc
scouri <eneblank19@protonmail.com>
2022-09-06 03:22:39 +0000

    different compression algos wip

--- src/compressor/compress.clj
+++ src/compressor/compress.clj
@@ -1,12 +1,33 @@
 (ns compressor.compress
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.java.io :refer [file input-stream output-stream copy] :as io])
   (:import [javax.imageio ImageIO IIOImage ImageWriteParam]
-           [java.util.zip ZipEntry ZipOutputStream]))
+           [java.util.zip ZipEntry ZipOutputStream]
+           [java.io File]
+           [org.apache.commons.compress.archivers.sevenz SevenZFile SevenZOutputFile]
+           [org.apache.commons.io FilenameUtils]))
 
 (defn file-extension
   [file]
   (second (re-find #"([^.][a-zA-Z0-9]+)$" file)))
+
+(def compressors ["bzip2" "deflate" "gz" "lz4-framed" "lzma" "xz"])
+
+(def archive-extensions
+  {"bzip2"      ".tar.bz2"
+   "deflate"    ".tar.gz"
+   "gz"         ".tar.gz"
+   "lz4-framed" ".tar.lz4"
+   "lzma"       ".tar.lzma"
+   "xz"         ".tar.xz"})
+
+(defn create-archive-name
+  [name output compressor]
+  (let [extension   (get archive-extensions compressor)
+        output-path (FilenameUtils/normalizeNoEndSeperator output)
+        output      (if (= File/seperator output-path) "" output-path)
+        output-file (str output File/seperator name extension)]
+    output-file))
 
 (defn compress-img
   [img & {:keys [quality new metadata]
@@ -14,8 +35,8 @@
   (let [ext (file-extension img)
         reader (.next (ImageIO/getImageReadersByFormatName ext))
         writer (.next (ImageIO/getImageWritersByFormatName ext))]
-    (with-open [input-stream (ImageIO/createImageInputStream (io/file img))
-                output-stream (ImageIO/createImageOutputStream (io/file new))]
+    (with-open [input-stream (ImageIO/createImageInputStream (file img))
+                output-stream (ImageIO/createImageOutputStream (file new))]
       (.setInput reader input-stream)
       (.setOutput writer output-stream)
       (let [image (if metadata (IIOImage. (.read reader 0) nil (.getImageMetadata reader 0)) (IIOImage. (.read reader 0) nil nil))
@@ -27,13 +48,14 @@
       (.dispose reader)
       (.dispose writer))))
 
-(defn compress-dir
+(defn zip-dir
   [dir & {:keys [output]
           :or {output dir}}]
-  (with-open [zip (ZipOutputStream. (io/output-stream output))]
-    (doseq [f (file-seq (io/file dir)) :when (.isFile f)]
+  (with-open [zip (ZipOutputStream. (output-stream output))]
+    (doseq [f (file-seq (file dir)) :when (.isFile f)]
       (.putNextEntry zip (ZipEntry. (str/replace-first (.getPath f) dir "")))
-      (io/copy f zip)
+      (copy f zip)
       (.closeEntry zip))))
+
 
 
